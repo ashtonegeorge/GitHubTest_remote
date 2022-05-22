8e2209c0022e3ef222d5e94828b3760323195198
Ashton George <ashtonsmail@icloud.com>
2022-05-22 18:23:13 -0400

    Initial Commit

---
+++ GitHubTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GitHubTest
+//
+//  Created by Ashton George on 5/22/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GitHubTest/GitHubTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubTestApp.swift
+//  GitHubTest
+//
+//  Created by Ashton George on 5/22/22.
+//
+
+import SwiftUI
+
+@main
+struct GitHubTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
