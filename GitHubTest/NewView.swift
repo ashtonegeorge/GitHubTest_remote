50900b2e6542ad8208d498f0d3ae19fff0665e71
Ashton George <ashtonsmail@icloud.com>
2022-05-22 18:28:45 -0400

    create second view

---
+++ GitHubTest/NewView.swift
@@ -0,0 +1,20 @@
+//
+//  NewView.swift
+//  GitHubTest
+//
+//  Created by Ashton George on 5/22/22.
+//
+
+import SwiftUI
+
+struct NewView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NewView_Previews: PreviewProvider {
+    static var previews: some View {
+        NewView()
+    }
+}
